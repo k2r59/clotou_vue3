e7e6ac840ca8f13370a9921511afd4867d96ee6c
clotilde lacaille <lacailleclotilde@gmail.com>
2022-10-17 10:11:11 +0200

    fix text error

--- src/components/WinPage.vue
+++ src/components/WinPage.vue
@@ -5,7 +5,7 @@
     fin mais il vous reste une dernière chose à accomplir.</p><br>
   <p>Veuillez retourner à la base, dans le premier espace dont le lien est ci-dessous.
     Vous devez changer la couleur des vêtements de votre avatar en bleu.
-    Ensuite dirigez vous au à l’étage inférieur.
+    Ensuite dirigez vous à l’étage inférieur.
     Alignez vous en ordre de dégradé de bleu contre le mur jonché du logo promo.dev.
     A tout de suite dans la base
   </p>
